73f41f0f748622b3ac51f60bf19d8665066c4c54
Hassan Murtaza <mrhassanmurtaza@gmail.com>
2019-03-26 17:25:59 +0500

    Setup Express Server

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require('express')
+
+let app = express()
+
+const PORT = 3000
+
+app.get('/test', (req, res) => {
+  res.json({ 'status': 200, 'message': 'success' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
